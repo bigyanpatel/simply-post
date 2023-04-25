f3f304965e26c047b76a710329068c528d882c4d
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2023-04-25 21:50:20 +0530

    Add: Title and meta to login

--- src/page/Authentication/LoginPage.jsx
+++ src/page/Authentication/LoginPage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { HeroSection, LoginForm } from "./Login/components/LoginPageExport";
+import { Helmet } from "react-helmet";
 
 export const LoginPage = () => {
   return (
     <div className="flex mt-8">
+      <Helmet>
+        <title>Login - SimplyPost</title>
+        <meta name="description" content="A simple social media app to share your thoughts on books." />
+        {/* Add more meta tags as needed */}
+      </Helmet>
       <div className="w-full p-4 m-auto md:flex lg:w-max">
         <HeroSection />
         <LoginForm />
